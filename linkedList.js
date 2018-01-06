e2654fb478d3e67fd5ee78811636af1b5f057173
levi <levi@levibrooke.com>
2018-01-05 21:27:02 -1000

    Complete through add, still working through get

--- linkedList.js
+++ linkedList.js
@@ -5,4 +5,90 @@
  */
 function linkedListGenerator(){
 
+  let head = null;
+  let tail = null;
+  let length = 0;
+
+  // create new node
+  function newNode(data) {
+    length++;
+    return {
+      value: data,
+      next: null
+    }
+  };
+
+  // getHead
+  function getHead() {
+    return head;
+  };
+
+  // getTail
+  function getTail() {
+    return tail;
+  };
+
+  // add(value)
+  function add(value) {
+    
+    // create a new node
+    let node = new newNode(value);
+
+    // when list is empty, assign first node to head and tail
+    if (head === null) {
+      head = node;
+      tail = node;
+      return node;
+    }
+    
+    // when list is not empty, append new node
+    if (head !== null) {
+      tail = node;
+      return node;
+    }
+    
+    return node;
+  };
+
+  // get(number)
+  function get(num) {
+    
+    // start at head
+    // check if value matches what the search is for
+    // if not found, move to next node via .next
+    // repeat until .next = null
+
+    let current = head;
+    let index = 0;
+
+    while (current !== null) {
+      if (index === num) {
+        return current;
+      } else {
+        index++;
+        current = current.next;
+      }
+    }
+    return current;
+  };
+
+  // remove(number)
+  function remove() {
+
+  };
+
+  // insert(value, number)
+  function insert() {
+
+  };
+
+  return {
+    getHead: getHead,
+    getTail: getTail,
+    add: add,
+    get: get,
+    remove: remove,
+    insert: insert
+  }
+
 }
